ba51ad456089033af2e4ef79fd20294b602809d4
handoiditu1508 <handoiditu1508@gmail.com>
2022-03-04 00:41:54 +0700

    update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,101 +6,98 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     {
-      name: "users",
-      title: "Users",
-      type: "document",
+      name: 'users',
+      title: 'Users',
+      type: 'document',
       fields: [
         {
-          name: "userName",
-          title: "User Name",
-          type: "string"
+          name: 'userName',
+          title: 'User Name',
+          type: 'string',
         },
         {
-          name: "walletAddress",
-          title: "Wallet Address",
-          type: "string"
+          name: 'walletAddress',
+          title: 'Wallet Address',
+          type: 'string',
         },
         {
-          name: "profileImage",
-          title: "Profile Image",
-          type: "image"
+          name: 'profileImage',
+          title: 'Profile Image',
+          type: 'image',
         },
         {
-          name: "bannerImage",
-          title: "Banner Image",
-          type: "image"
+          name: 'bannerImage',
+          title: 'Banner Image',
+          type: 'image',
         },
         {
-          name: "twitterHandle",
-          title: "Twitter Handle",
-          type: "string"
+          name: 'twitterHandle',
+          title: 'Twitter Handle',
+          type: 'string',
         },
         {
-          name: "igHandle",
-          title: "Instagram Handle",
-          type: "string"
-        }
-      ]
+          name: 'igHandle',
+          title: 'Instagram Handle',
+          type: 'string',
+        },
+      ],
     },
     {
-      name: "marketItems",
-      title: "Market Items",
-      type: "document",
-      fields:[
+      name: 'marketItems',
+      title: 'Market Items',
+      type: 'document',
+      fields: [
         {
-          name: "title",
-          title: "Title",
-          type: "string"
+          name: 'title',
+          title: 'Title',
+          type: 'string',
         },
         {
-          name: "contractAddress",
-          title: "Contract Address",
-          type: "string"
+          name: 'contractAddress',
+          title: 'Contract Address',
+          type: 'string',
         },
         {
-          name: "description",
-          title: "Description",
-          type: "string"
+          name: 'description',
+          title: 'Description',
+          type: 'string',
         },
         {
-          name: "createdBy",
-          title: "Created By",
-          type: "reference",
-          to: [{type: "users"}]
+          name: 'createdBy',
+          title: 'Created By',
+          type: 'reference',
+          to: [{ type: 'users' }],
         },
         {
-          name: "volumeTraded",
-          title: "Volume Traded",
-          type: "number"
+          name: 'volumeTraded',
+          title: 'Volume Traded',
+          type: 'number',
         },
         {
-          name: "floorPrice",
-          title: "Floor Price",
-          type: "number"
+          name: 'floorPrice',
+          title: 'Floor Price',
+          type: 'number',
         },
         {
-          name: "owners",
-          title: "Owners",
-          type: "array",
-          of: [{type: "reference", to: [{type: "users"}]}]
+          name: 'owners',
+          title: 'Owners',
+          type: 'array',
+          of: [{ type: 'reference', to: [{ type: 'users' }] }],
         },
         {
-          name: "profileImage",
-          title: "Profile Image",
-          type: "image"
+          name: 'profileImage',
+          title: 'Profile Image',
+          type: 'image',
         },
         {
-          name: "bannerImage",
-          title: "Banner Image",
-          type: "image"
-        }
-      ]
-    }
+          name: 'bannerImage',
+          title: 'Banner Image',
+          type: 'image',
+        },
+      ],
+    },
   ]),
 })
